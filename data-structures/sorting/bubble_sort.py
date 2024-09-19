373cef7058c7886cd7dd337d9505eabe939103cb
Jai Deep Sharma <jaideep.sharma.ext@orange.com>
2024-09-19 23:31:05 +0530

    optimize to exclulde last largest element moved from the next iteration

--- data-structures/sorting/bubble_sort.py
+++ data-structures/sorting/bubble_sort.py
@@ -26,22 +26,19 @@ def bubble_sort(nums):
 
 
 def bubble_sort2(nums):
-    for _ in range(len(nums)):
-        for i in range(len(nums) - 1):
+    print(nums, " <-input")
+    for j in range(len(nums)):
+        for i in range(len(nums) - 1 - j):
             p_val = nums[i]
             nxt_val = nums[i + 1]
             if p_val > nxt_val:
                 p_val, nxt_val = nxt_val, p_val
                 nums[i] = p_val
                 nums[i + 1] = nxt_val
+    print(nums)
     return nums
 
 
-t = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-
-
-print(t, " <-input")
-# print(bubble_sort2(t) == [17, 20, 26, 31, 44, 54, 55, 77, 93])
 assert bubble_sort2([54, 26, 93, 17, 77, 31, 44, 55, 20]) == [
     17,
     20,
@@ -57,4 +54,3 @@ assert bubble_sort2([4, 1, 3, 4, 2, 2]) == [1, 2, 2, 3, 4, 4]
 assert bubble_sort2([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
 assert bubble_sort2([42]) == [42]
 assert bubble_sort2([]) == []
-
